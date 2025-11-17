4655b7e56b064fa37cc134c122e25a03f3261f38
Clement <clement@gmail.com>
2025-11-17 10:34:40 -0500

    init_commit

---
+++ frontend/calm/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+
+export const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
